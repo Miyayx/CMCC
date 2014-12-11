3b152f877e2036269571fe767646a0db3ae45449
Miyayx <yangxue00.yxmn@gmail.com>
2014-12-11 16:58:37 +0800

    delete feature path

--- table_preprocess.py
+++ table_preprocess.py
@@ -34,14 +34,11 @@ def run(configs, file_cfg):
 
     import os
     OTHERS_PATH = os.path.join(RESULT_PATH, file_configs["others_output_path"])
-    FEATURE_PATH = os.path.join(RESULT_PATH, file_configs["feature_output_path"])
 
     if not os.path.isdir(RESULT_PATH):
         os.mkdir(RESULT_PATH)
     if not os.path.isdir(OTHERS_PATH):
         os.mkdir(OTHERS_PATH)
-    if not os.path.isdir(FEATURE_PATH):
-        os.mkdir(FEATURE_PATH)
     if not os.path.isdir(LOG_PATH):
         os.mkdir(LOG_PATH)
 
@@ -128,13 +125,6 @@ def run(configs, file_cfg):
         features.append(h_feature)
 
         record_left_label(o_table_header, headers, left_tableheader_file)
-
-#####################################################  feature file output  ########################################
-
-    #outfile = os.path.join(FEATURE_PATH, section+".csv")
-
-    #先写个原始的feature文件
-    #write_dataset(sample_block, feature_fields(fields), features, class_block, fconfigs["split"], outfile)
 
     ############## record feature count ##############
     if fconfigs["section_label"] or fconfigs["block_label"] or fconfigs["table_header"]:
