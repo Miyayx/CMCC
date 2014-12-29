5c3cf148643de9e6366864a0113bb35f89bbecd2
Miyayx <yangxue00.yxmn@gmail.com>
2014-12-29 11:11:10 +0800

    change section validation

--- section_validation.py
+++ section_validation.py
@@ -13,19 +13,24 @@ class SectionValidation(Validation):
     
         prop = read_properties(file_col) #get feature count from file_col file
     
+    
         options = {
-            "block":{
+            "section":{
                 "begin":2,
-                "end":2+int(prop["block_count"]),
-                "left_file":os.path.join(OTHERS_PATH, props['left_block_file']+'_features'+str(Y)+'.dat')
+                "end":2+int(prop["section_count"]),
+                "left_file":os.path.join(OTHERS_PATH, props['left_section_file']+'_features'+str(Y)+'.dat')
             },
+            "block":{
+                "begin":2+int(prop["section_count"]),
+                "end":  2+int(prop["section_count"])+int(prop["block_count"]),
+                "left_file":os.path.join(OTHERS_PATH, props['left_block_file']+'_features'+str(Y)+'.dat')
+                },
             "table_header":{
-                "begin":2+int(prop["block_count"]),
-                "end":  2+int(prop["block_count"])+int(prop["table_header"]),
+                "begin":2+int(prop["section_count"])+int(prop["block_count"]),
+                "end":  2+int(prop["section_count"])+int(prop["block_count"])+int(prop["table_header"]),
                 "left_file":os.path.join(OTHERS_PATH, props['left_tableheader_file']+'_features'+str(Y)+'.dat')
                 }
-            }
-    
+        }
         self.validation(feature_file,options)
 
 if __name__=="__main__":
